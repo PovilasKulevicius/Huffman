c77d542368c505bd61b985a7918cba75557bfba1
TomasMacikas <macikastomas@gmail.com>
2018-12-14 16:47:08 +0200

    Test environment update

--- Huffman Algorythm/src/Testing.java
+++ Huffman Algorythm/src/Testing.java
@@ -1,19 +1,42 @@
 import java.io.*;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Testing {
     public static void main(String[] args) throws IOException {
         String[] compress = new String[3];
         String[] decompress = new String[2];
-        compress[1] = "pav.jpg";
-        for(int k = 2; k < 25; ++k){
+
+        String fileName = "test.txt";
+        Path path = Paths.get("C:/Users/Tomas/IdeaProjects/Huffman", fileName);
+        byte[] f1 = Files.readAllBytes(path);
+        compress[1] = fileName;
+        for(int k = 3; k < 25; ++k){
+            String compressedName = "compressed" + Integer.toString(k);
+            String decompressedName = "decompressed" + Integer.toString(k) + ".txt";
+
             compress[0] = Integer.toString(k);
-            compress[2] = "compressed" + Integer.toString(k);
+            compress[2] = compressedName;
             HuffmanCompress.main(compress);
-            decompress[0] = "compressed" + Integer.toString(k);
-            decompress[1] = "decompressed" + Integer.toString(k) + ".jpg";
+
+            Path path2 = Paths.get("C:/Users/Tomas/IdeaProjects/Huffman", compressedName);
+            byte[] f2 = Files.readAllBytes(path2);
+
+            decompress[0] = compressedName;
+            decompress[1] = decompressedName;
             HuffmanDecompress.main(decompress);
+            //Check if files are same
+            Path path3 = Paths.get("C:/Users/Tomas/IdeaProjects/Huffman", decompressedName);
+            byte[] f3 = Files.readAllBytes(path3);
+            if(Arrays.equals(f1, f3)){
+                System.out.println("With " + k + " bits " + "Pre-compressed/compressed " + f1.length + "/" + f2.length + " Passed");
+            }
+            else System.out.println("With " + k + " bits " + "Pre-compressed/compressed " + f1.length + "/" + f2.length + " FAILED");
         }
     }
 }
